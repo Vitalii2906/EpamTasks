af72c89d3fec39b9655e0bf0ea23ac1da71982d5
One-of-the <fahivchik1@gmail.com>
2020-04-22 21:04:01 +0300

    end tusk1

---
+++ FL13_HW6/homework/js/task1.js
@@ -0,0 +1,20 @@
+// Your code goes here
+let check = Number(window.prompt('Check number'));
+let tip = Number(window.prompt('Tip'));
+
+
+if((typeof check === 'number' && typeof tip === 'number')&&((check >= 0) && (tip >= 0 && tip <=100))){
+   let procent = tip / 100;
+   let sumAmount = procent * check;
+   let sum = check + sumAmount; 
+   if(sum >= 0){
+      window.alert(`
+      Check number: ${check}
+      Tip: ${tip}%
+      Tip amount: ${sumAmount}
+      Total sum to pay: ${sum}
+      `)
+   }
+} else{
+   alert("Invalid input data")
+};
