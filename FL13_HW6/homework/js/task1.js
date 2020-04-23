2ba04df90ab05672d84d9eecd51d01821c88eeb7
One-of-the <fahivchik1@gmail.com>
2020-04-23 20:53:56 +0300

    ended homework

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,12 +1,12 @@
-// Your code goes here
-let check = Number(window.prompt('Check number'));
-let tip = Number(window.prompt('Tip %'));
 
+let check = Number(window.prompt('Check number'))
+let tip = Number(window.prompt('Tip %'))
+let oneHundred = 100
 
-if((typeof check === 'number' && typeof tip === 'number')&&((check >= 0) && (tip >= 0 && tip <=100))){
-   let procent = tip / 100;
-   let sumAmount = procent * check;
-   let sum = check + sumAmount; 
+if(typeof check === 'number' && typeof tip === 'number' && check >= 0 && tip >= 0 && tip <= oneHundred){
+   let procent = tip / oneHundred
+   let sumAmount = procent * check
+   let sum = check + sumAmount 
    if(sum >= 0){
       window.alert(`
       Check number: ${check}
@@ -16,5 +16,5 @@ if((typeof check === 'number' && typeof tip === 'number')&&((check >= 0) && (tip
       `)
    }
 } else{
-   alert("Invalid input data")
-};
+   alert('Invalid input data')
+}
--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let word = window.prompt('Write the word')
+let two = 2
+
+function getMiddle(str) {
+   let arr = str.split('');
+   while (arr.length > two) {
+      arr.pop();
+      arr.shift();
+   }
+   return arr.join('');
+}
+
+
+if(word.length !== 0 && word.trim() !== ''){
+   alert(getMiddle(word))
+}else{
+   alert('Invalid value')
+}
+
+
