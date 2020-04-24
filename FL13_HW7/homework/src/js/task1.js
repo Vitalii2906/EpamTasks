500c1a2ef2967702977455cd4fa52d4f50e8c052
One-of-the <fahivchik1@gmail.com>
2020-04-24 15:18:33 +0300

    end first task

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,1 +1,37 @@
-// Your code goes here
+
+let checkLogin = window.prompt('Login')
+let checkPassword
+let four = 4
+let twenty = 20.00
+let date = new Date();
+let hours = date.getHours()
+
+
+
+if(checkLogin){
+   if(checkLogin !== 0 && checkLogin === 'User' || checkLogin === 'Admin'){
+      checkPassword = prompt('Password')
+      if( checkPassword === 'UserPass' ){
+         if(hours < Number(twenty)){
+            alert('Good day, dear User!')
+         }else {
+            alert('Good evening, dear User!')
+         }
+      }else if( checkPassword === 'AdminPas' ){
+         if(hours < Number(twenty)){
+            alert('Good day, dear Admin!')
+         }else{
+            alert('Good evening, dear Admin!')
+         }
+      }else{
+         alert('Wrong password')
+      }
+   }else if(checkLogin.length > Number(four)){
+      alert('I dont know any users')
+   }else if(checkLogin.length <= Number(four)){
+      alert('I don’t know you')
+   }
+}else{
+   alert('Canceled.')
+}
+
