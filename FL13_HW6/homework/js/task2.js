b2c5b3a7e845902a0ee469b2fd0180c1d7392a88
One-of-the <fahivchik1@gmail.com>
2020-04-24 09:38:31 +0300

    add

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,18 +1,18 @@
 let word = window.prompt('Write the word')
 let two = 2
 
-function getMiddle(str) {
-   let arr = str.split('');
+function middleWords(str) {
+   let arr = str.split('')
    while (arr.length > two) {
-      arr.pop();
-      arr.shift();
+      arr.pop()
+      arr.shift()
    }
-   return arr.join('');
+   return arr.join('')
 }
 
 
 if(word.length !== 0 && word.trim() !== ''){
-   alert(getMiddle(word))
+   alert(middleWords(word))
 }else{
    alert('Invalid value')
 }
