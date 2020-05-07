13e5b3ed4cc53bd4ebe029c2020adeadb3cae5cc
One-of-the <fahivchik1@gmail.com>
2020-05-07 19:12:29 +0300

    start

---
+++ FL13_HW10/homework/js/fighters_game.js
@@ -0,0 +1,15 @@
+
+
+
+
+function Fighter({}){
+   
+}
+function getName(object){
+   let name = object.
+   return  name
+}
+
+const fighter = new Fighter({name: 'Andriy'})
+
+console.log(getName(fighter))
