fe6e55315c0504ff53fbdc3e9cc11ab526996ff7
One-of-the <fahivchik1@gmail.com>
2020-05-21 14:55:26 +0300

    finish first function

--- FL13_HW14/homework/js/app.js
+++ FL13_HW14/homework/js/app.js
@@ -1,1 +1,19 @@
-// Your code goes here
+function Student(name, email){
+   let homeworkResult = []
+   this.getName = function(){
+      this.name = name
+      return name
+   }
+   this.getEmail = function(){
+      this.email = email
+      return email
+      
+   }
+   this.addHomeworkResult = function(topic, success){
+      this.topic = topic
+      homeworkResult.push(topic)
+      this.success = success
+      homeworkResult.push(success)
+      return homeworkResult
+   }
+}
