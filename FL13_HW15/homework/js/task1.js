94ccdd57cb4efb4405adc7844cfd0e48dd4d3b8f
One-of-the <fahivchik1@gmail.com>
2020-05-25 22:03:27 +0300

    add 1 task

---
+++ FL13_HW15/homework/js/task1.js
@@ -0,0 +1,12 @@
+
+function myAssign(target, ...sources) {
+   sources.forEach(source => {
+      Object.defineProperties(target, Object.keys(source).reduce((descriptors, key) => {
+         descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
+         return descriptors;
+      }, {}));
+   });
+   return target;
+}
+
+
