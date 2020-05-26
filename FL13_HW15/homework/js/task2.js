c6eb36940c62f9cb2404c3ae2938c2dd8ddf15ff
One-of-the <fahivchik1@gmail.com>
2020-05-26 20:51:42 +0300

    add

--- FL13_HW15/homework/js/task2.js
+++ FL13_HW15/homework/js/task2.js
@@ -7,12 +7,11 @@ function Vehicle(color, engine) {
 	this.isDriving = false
 	this.isStopping = false
 	this.isStopped = true
-	this.lastMaxSpeed = 0
-	this.currSpeed = 0
    this.interval
    this.name = 'Vehicle'
    this.maxSpeed = 70
    this.model = 'unknown model'
+   this.currSpeed = 0
 
 	this.getInfo = function() {
 		return{
@@ -40,34 +39,34 @@ function Vehicle(color, engine) {
 			this.isStopping = false
 			this.isDriving = true
 			this.interval = setInterval(() => {
-				this.currSpeed += 20
+            this.currSpeed += 20
 				console.log(this.currSpeed)
 				if(this.currSpeed >= this.maxSpeed && !this.isStopping) {
 					console.log('speed is too high, SLOW DOWN!')
 				}
-			}, twoThousand)
+         }, twoThousand)
 		}
 	},
 
 	this.stop = function() {
-      this.stopMessage = `${this.name} is stopped. Maximum speed during the drive was ${
-         this.currSpeed}`
       this.lastMaxSpeed = this.currSpeed
+      this.stopMessage = `${this.name} is stopped. Maximum speed during the drive was ${
+         this.lastMaxSpeed}`
 		this.isStopping = true
 		this.isDriving = false
 		clearInterval(this.interval)
 		this.interval = setInterval( () => {
-			this.lastMaxSpeed -= 20
-			console.log(this.lastMaxSpeed)
-			if(this.lastMaxSpeed <= 0) {
+			this.currSpeed -= 20
+			console.log(this.currSpeed)
+			if(this.currSpeed <= 0) {
 				clearInterval(this.interval)
-				this.lastMaxSpeed = 0
+				this.currSpeed = 0
 				this.isStopping = false
 				this.isStopped = true
 				console.log(this.stopMessage)
 			}
 		}, oneThousandAndFiveHundred)
-	}
+   }
 }
 
 function Car(model, color, engine){
@@ -85,6 +84,25 @@ function Car(model, color, engine){
          console.log('The car is now this color')
       }
    }
+   this.stop = function() {
+      this.lastMaxSpeed = this.currSpeed
+      this.stopMessage = `${this.name} ${this.model} is stopped. Maximum speed during the drive ${
+         this.lastMaxSpeed}`
+		this.isStopping = true
+		this.isDriving = false
+		clearInterval(this.interval)
+		this.interval = setInterval( () => {
+			this.currSpeed -= 20
+			console.log(this.currSpeed)
+			if(this.currSpeed <= 0) {
+				clearInterval(this.interval)
+				this.currSpeed = 0
+				this.isStopping = false
+				this.isStopped = true
+				console.log(this.stopMessage)
+			}
+		}, oneThousandAndFiveHundred)
+   }
 }
 Car.prototype = new Vehicle()
 
@@ -96,5 +114,23 @@ function Motorcycle(model, color, engine){
    this.model = model
    this.maxSpeed = 90
    this.name = 'Motorcycle'
+   this.stop = function() {
+      this.lastMaxSpeed = this.currSpeed
+      this.stopMessage = `${this.name} ${this.model} is stopped. Good drive`
+		this.isStopping = true
+		this.isDriving = false
+		clearInterval(this.interval)
+		this.interval = setInterval( () => {
+			this.currSpeed -= 20
+			console.log(this.currSpeed)
+			if(this.currSpeed <= 0) {
+				clearInterval(this.interval)
+				this.currSpeed = 0
+				this.isStopping = false
+				this.isStopped = true
+				console.log(this.stopMessage)
+			}
+		}, oneThousandAndFiveHundred)
+   }
 }
 Motorcycle.prototype = new Car()
